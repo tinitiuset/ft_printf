e7c080297dd352d8ac030c878562c0d3d8c6a887
tinitiuset <tinitiuset@gmail.com>
2022-09-13 12:54:27 +0200

    Text parsing done

--- ft_printf.c
+++ ft_printf.c
@@ -6,36 +6,45 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 09:18:28 by mvalient          #+#    #+#             */
-/*   Updated: 2022/09/13 11:47:45 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/13 12:50:33 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	convert_text(char const *s)
+int	ft_convert_text(char const *s, void *arg)
+{
+	char			*sarg;
+	unsigned int	i;
+
+	if (*s == 'c')
+		return (write(1, &arg, 1));
+	i = -1;
+	sarg = (char *)arg;
+	while (sarg[++i])
+		write (1, &sarg[i], 1);
+	return (i + 1);
+}
+
+int	ft_convert_dec(char const *s)
 {
 	return (0);
 }
 
-int	convert_dec(char const *s)
+int	ft_convert_hexa(char const *s)
 {
 	return (0);
 }
 
-int	convert_hexa(char const *s)
-{
-	return (0);
-}
-
-int	convert_filter(char const *s)
+int	ft_convert_filter(char const *s, va_list args)
 {
 	s++;
 	if (*s == 'c' || *s == 's')
-		return (convert_text(s));
+		return (ft_convert_text(s, va_arg(args, char *)));
 	if (*s == 'd' || *s == 'i' || *s == 'u')
-		return (convert_dec(s));
+		return (ft_convert_dec(s));
 	if (*s == 'p' || *s == 'x' || *s == 'X')
-		return (convert_hexa(s));
+		return (ft_convert_hexa(s));
 	if (*s == '%')
 	{
 		write (1, "%", 1);
@@ -46,6 +55,9 @@ int	convert_filter(char const *s)
 
 int	ft_printf(char const *s, ...)
 {
+	va_list	args;
+
+	va_start(args, s);
 	while (*s)
 	{
 		if (*s != '%')
@@ -54,9 +66,10 @@ int	ft_printf(char const *s, ...)
 		}
 		else
 		{
-			s = (s + convert_filter(s));
+			s = (s + ft_convert_filter(s, args));
 		}	
 		s++;
 	}
+	va_end(args);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 12:00:14 by mvalient          #+#    #+#             */
-/*   Updated: 2022/09/13 12:00:08 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/13 12:19:39 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include <stdio.h>
 
 int	ft_printf(char const *s, ...);
 
